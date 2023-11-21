85c973c0c878292f1fbf059af03cce41651159d4
StefaniStoycheva <stefani.stoycheva@gmail.com>
2023-11-21 19:17:31 +0200

    Dictionaries

---
+++ DictionariesLambdaLINQ-Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberOfPairs = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<string>> synonims = new();
+
+for (int i = 0; i < numberOfPairs; i += 1)
+{
+    string word = Console.ReadLine();
+    string synonim = Console.ReadLine();
+
+    if (synonims.ContainsKey(word))
+    {
+        synonims[word].Add(synonim);
+    }
+    else
+    {
+        synonims.Add(word, new List<string> { synonim });
+    }
+}
+foreach (KeyValuePair<string, List<string>> pair in synonims)
+{
+    Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+}
