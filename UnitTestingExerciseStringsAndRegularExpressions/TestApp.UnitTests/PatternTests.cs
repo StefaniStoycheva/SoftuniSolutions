2a96da193958270c45dc8625df54fe8bd1684b21
StefaniStoycheva <stefani.stoycheva@gmail.com>
2023-11-24 16:39:48 +0200

    Unit Testing

---
+++ UnitTestingExerciseStringsAndRegularExpressions/TestApp.UnitTests/PatternTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PatternTests
+{
+    
+    [TestCase("abcdef", 2, "aBcDeFaBcDeF")]
+    [TestCase("ABCDEF", 1, "aBcDeF")]  
+    [TestCase("abcdef", 10, "aBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeFaBcDeF")]
+    public void Test_GeneratePatternedString_ValidInput_ReturnsExpectedResult(string input, 
+        int repetitionFactor, string expected)
+    {
+        // Arrange 
+
+        // Act
+        string result = Pattern.GeneratePatternedString(input, repetitionFactor);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_EmptyInput_ThrowsArgumentException()
+    {
+        // Arrange 
+        string input = "";
+        int repetiotionFactor = 10; 
+
+        // Act & Assert
+        Assert.That(()=> Pattern.GeneratePatternedString(input, repetiotionFactor), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_NegativeRepetitionFactor_ThrowsArgumentException()
+    {
+        // Arrange 
+        string input = "abcd";
+        int repetiotionFactor = -5;
+
+        // Act & Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetiotionFactor), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_GeneratePatternedString_ZeroRepetitionFactor_ThrowsArgumentException()
+    {
+        // Arrange 
+        string input = "abcd";
+        int repetiotionFactor = 0;
+
+        // Act & Assert
+        Assert.That(() => Pattern.GeneratePatternedString(input, repetiotionFactor), Throws.ArgumentException);
+    }
+}
---
+++ UnitTestingExerciseStringsAndRegularExpressions/TestApp.UnitTests/ReverseConcatenateTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Linq;
+
+namespace TestApp.UnitTests;
+
+public class ReverseConcatenateTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_EmptyInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string[] input = Array.Empty<string>();
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish the test
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_SingleString_ReturnsSameString()
+    {
+        // Arrange
+        string[] input = new[] { "blabla" };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("blabla")) ;
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_MultipleStrings_ReturnsReversedConcatenatedString()
+    {
+        // Arrange
+        string[] input = new[] { "blabla", "bla", "bl" };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("blblablabla"));
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_NullInput_ReturnsEmptyString()
+    {
+        // Arrange
+        string[] input = null;
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_WhitespaceInput_ReturnsConcatenatedString()
+    {
+        // Arrange
+        string[] input = new[] { " ", "  ", "   " };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("      "));
+    }
+
+    [Test]
+    public void Test_ReverseAndConcatenateStrings_LargeInput_ReturnsReversedConcatenatedString()
+    {
+        // Arrange
+        string[] input = new[] { "blabla", "bla", "bl", "blabla", "bla", "bl", "blabla", "bla", "bl", "blabla", "bla", "bl" };
+
+        // Act
+        string result = ReverseConcatenate.ReverseAndConcatenateStrings(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("blblablablablblablablablblablablablblablabla"));
+    }
+}
