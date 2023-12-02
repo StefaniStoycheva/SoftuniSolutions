38ce3db07e5f1157010c8455d56b4089675413f2
StefaniStoycheva <stefani.stoycheva@gmail.com>
2023-12-02 19:05:00 +0200

    ExceptionHandlingLab

---
+++ ExceptionHandling/1. Square Root/Program.cs
@@ -0,0 +1,20 @@
+﻿
+try
+{
+    int num = int.Parse(Console.ReadLine());
+    if (num < 0)
+    {
+        throw new ArgumentException();  //causing the problem
+    }
+
+    double sqrt = Math.Sqrt(num); 
+    Console.WriteLine(sqrt);
+}
+catch(ArgumentException)
+{
+    Console.WriteLine("Invalid number."); //catching it 
+}
+finally
+{
+    Console.WriteLine("Goodbye."); //always write/execute
+}
